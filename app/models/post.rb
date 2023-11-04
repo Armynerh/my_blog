e271b46c3b32f3b717eb1bb4f7d0960e7446b6ab
Armynerh <aimenah@outlook.com>
2023-11-04 00:07:39 -0400

    add file for data model processing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :author_id
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  after_create :update_user_posts_counter
+
+  def update_user_posts_counter
+    user.increment!(:posts_counter)
+  end
 end
