c93cd64bd7bdd1a20904c7933a55293dce2d70d0
Armynerh <aimenah@outlook.com>
2023-11-09 15:01:34 -0500

    make review changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index]
+    resources :posts, only: [:index, :show]
   end
-  resources :posts, only: [:show], as: 'user_post'
   # Defines the root path route ("/")
   # root "posts#index"
 end
