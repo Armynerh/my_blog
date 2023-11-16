3021df73b79ca5be910c9446616a31458ab60f31
Armynerh <aimenah@outlook.com>
2023-11-15 20:39:19 -0500

    make changes for views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
--- db/schema.rb
+++ db/schema.rb
@@ -34,14 +34,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_04_173419) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "author_id_id", null: false
+    t.bigint "author_id", null: false
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id_id"], name: "index_posts_on_author_id_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,6 +56,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_04_173419) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users", column: "author_id_id"
+  add_foreign_key "likes", "users" 
+  add_foreign_key "posts", "users", column: "author_id"
 end
