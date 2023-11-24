d2d0d93e39d5ba8f7305f491c2af0a47b557f375
EL MEHDI AHMIDOU <ahmidouem@gmail.com>
2023-11-24 17:24:09 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,1 @@
-class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  include Devise::Controllers::Helpers
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation bio photo])
-  end
-end
